efca6ee79074685513f4beb66f6b416c1d8eaf8b
NhatNamITPRO <nhatnamit214@gmail.com>
2024-12-09 15:24:32 +0700

    fix mode

--- app.py
+++ app.py
@@ -130,5 +130,5 @@ def predict():
     return jsonify({'error': 'Invalid request'}), 400
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
